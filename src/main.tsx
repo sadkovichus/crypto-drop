30aea8ab4ddfe4f8b4d67fa02acef73f9c8b1674
Georgiy <sadkovdanil64@gmail.com>
2024-12-18 13:19:33 +0300

    chore

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
+import App from '@app/'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
